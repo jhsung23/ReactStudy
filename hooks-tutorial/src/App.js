e23294cfc8078c357846996b55bb845b6f4080e3
sungjihyun <jhsung23@gmail.com>
2023-01-31 23:01:33 +0900

    feat(8장): useMemo

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,7 @@
-import { Counter } from './Counter';
-import { Info } from './Info';
+import { Average } from './Average';
 
 function App() {
-  return <Info />;
+  return <Average />;
 }
 
 export default App;
