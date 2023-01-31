84d4fd0ddad773b4440c13a97dbc38828c9f90e7
sungjihyun <jhsung23@gmail.com>
2023-01-31 13:53:45 +0900

    feat(7장): 에러 잡아내기

--- hello-react/src/LifeCycleSample.js
+++ hello-react/src/LifeCycleSample.js
@@ -64,6 +64,7 @@ export default class LifeCycleSample extends Component {
 
     return (
       <div>
+        {this.props.missing.value}
         <h1 style={style} ref={(ref) => (this.myRef = ref)}>
           {this.state.number}
         </h1>
