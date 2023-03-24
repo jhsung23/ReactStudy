725c1588d45e7bb867117c89d4be8255d244401d
sungjihyun <jhsung23@gmail.com>
2023-03-24 21:11:45 +0900

    feat: 라우팅

--- practice/velog-cloning/src/App.js
+++ practice/velog-cloning/src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
-import Header from './Header.js';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './Layout';
+import Articles from './pages/Articles';
+import Series from './pages/Series';
+import About from './pages/About';
 
 function App() {
-  return <Header />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Articles />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
