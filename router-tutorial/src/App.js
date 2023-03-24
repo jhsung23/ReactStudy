19d86c5d045137618b72d081bdcd50fa3cb07bef
sungjihyun <jhsung23@gmail.com>
2023-03-24 11:17:02 +0900

    feat(13장): Route, Routes

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Articles from './pages/Articles';
+import Article from './pages/Article';
+import Layout from './Layout';
+
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import MyPage from './pages/MyPage';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;
