62a2845d410ab803b74d1f5c575d42bdb76bc78d
sungjihyun <jhsung23@gmail.com>
2023-04-24 10:06:12 +0900

    feat(16장): 액션 타입과 액션 생성 함수 정의

---
+++ vanilla-redux/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// Action 생성 함수
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
