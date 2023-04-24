59508da62ea059b644b44f010b9ac70e3349f516
sungjihyun <jhsung23@gmail.com>
2023-04-24 10:07:05 +0900

    feat(16장): 리듀서 함수 정의

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -18,3 +18,26 @@ const initialState = {
   toggle: false,
   counter: 0,
 };
+
+// reducer : 새로운 상태를 만들어 반환한다.
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
