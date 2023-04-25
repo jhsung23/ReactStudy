7989fd6039c5dc4b0a0dc1843820bd607be27a14
sungjihyun <jhsung23@gmail.com>
2023-04-25 14:32:04 +0900

    refactor(17장): bindActionCreators와 일반 객체로 mapDispatchToProps 간소화

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,3 +1,4 @@
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -33,4 +34,20 @@ export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 //       dispatch(decrease());
 //     },
 //   }),
+// )(CounterContainer);
+
+// 방법 3. bindActionCreators 사용
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   (dispatch) => bindActionCreators({ increase, decrease }, dispatch),
+// )(CounterContainer);
+
+// 방법 4. dispatch 함수 대신 객체 형태로 넣어주기
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   { increase, decrease },
 // )(CounterContainer);
